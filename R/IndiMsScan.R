8f4b77404797b9567e8c9012931a91b40da87b26
Dr. Reema Singh <ReemaSingh@users.noreply.github.com>
2025-09-16 10:02:56 -0600

    Update IndiMsScan.R

--- R/IndiMsScan.R
+++ R/IndiMsScan.R
@@ -1,3 +1,6 @@
+#################################################
+# Written by Dr. Reema Singh #
+#################################################
 IndiMsScan <- function(x,i){
         y <- data.frame(x[[i]]$b)
         colnames(y) <- c("mass","absolute","relative")
