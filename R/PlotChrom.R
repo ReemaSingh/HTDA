b9c44b67eb8eb49dcf53a0c72ce7a459cbd1e952
Dr. Reema Singh <ReemaSingh@users.noreply.github.com>
2025-09-16 10:04:05 -0600

    Update PlotChrom.R

--- R/PlotChrom.R
+++ R/PlotChrom.R
@@ -1,3 +1,6 @@
+#################################################
+# Written by Dr. Reema Singh #
+#################################################
 PlotChrom <- function(x){
         plot(as.character(x$RT),as.character(x$MZ),type="h",xlab="Retention Time", ylab = "Mass/Charge Ration")
         identify(as.character(x$RT),as.character(x$MZ),x$MSPeaks)
